4545d402c420c78f48aeb7609630fdc2fe417cff
Qasim <qasim321200@gmail.com>
2025-07-22 13:06:37 +0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,13 +2,16 @@ let express = require('express');
 let app = express();
 
 
-app.get(function(req, res) {
-   res.send("Hello World")
+app.get('/',(req,res)=>{
+    res.send("Hello Express")
 })
 
 
 
-
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
+})
 
 
 
