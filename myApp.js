8388df484153c671fd88e80f3c0eca1acf5b60ee
Qasim <qasim321200@gmail.com>
2025-07-22 13:12:47 +0500

    fixes

--- myApp.js
+++ myApp.js
@@ -4,17 +4,9 @@ let app = express();
 
 app.get('/',(req,res)=>{
     res.send("Hello Express")
-})
+});
 
-
-
-const port = 3000;
-app.listen(port, ()=>{
-    console.log(`Server running on port ${port}`);
-})
-
-
-
+module.exports = app;
 
 
 
